1821f0c6dd049fb487d43cd3b46465e0d17cd049
chengxiaoxiao9986 <59251490@qq.com>
2016-10-25 19:17:05 +0800

    10.25

---
+++ app/src/main/java/cn/edu/pku/chengxiaoxiao/bean/TodayWeather.java
@@ -0,0 +1,8 @@
+package cn.edu.pku.chengxiaoxiao.bean;
+
+/**
+ * Created by Jason on 16/10/22.
+ */
+
+public class TodayWeather {
+}
---
+++ app/src/main/java/cn/edu/pku/chengxiaoxiao/weather/SelectCtiy.java
@@ -0,0 +1,8 @@
+package cn.edu.pku.chengxiaoxiao.weather;
+
+/**
+ * Created by Jason on 16/10/23.
+ */
+
+public class SelectCtiy {
+}
