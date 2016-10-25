1a6f2cff738236701c6f01790e310c2a91879275
chengxiaoxiao9986 <59251490@qq.com>
2016-10-25 19:23:59 +0800

    10-25

--- app/src/main/java/cn/edu/pku/chengxiaoxiao/bean/TodayWeather.java
+++ app/src/main/java/cn/edu/pku/chengxiaoxiao/bean/TodayWeather.java
@@ -5,4 +5,145 @@ package cn.edu.pku.chengxiaoxiao.bean;
  */
 
 public class TodayWeather {
+
+
+
+
+    private String city;
+    private String updatetime;
+    private String wendu;
+    private String shidu;
+    private String pm25;
+    private String quality;
+    private String fengxiang;
+    private String fengli;
+    private String date;
+    private String high;
+    private String low;
+    private String type;
+
+
+
+    public String getType() {
+        return type;
+    }
+
+    public String getLow() {
+
+        return low;
+    }
+
+
+    public String getHigh() {
+
+        return high;
+    }
+    public String getCity() {
+        return city;
+    }
+
+    public String getUpdatetime() {
+        return updatetime;
+    }
+
+    public String getWendu() {
+        return wendu;
+    }
+
+    public String getShidu() {
+        return shidu;
+    }
+
+    public String getPm25() {
+        return pm25;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public String getFengxiang(){
+        return fengxiang;
+    }
+    public String getFengli() {
+        return fengli;
+    }
+    public String getDate() {
+        return date;
+    }
+
+
+
+    public void setFengli(String fengli) {
+        this.fengli = fengli;
+    }
+
+    public void setCity(String city) {
+
+        this.city = city;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public void setFengxiang(String fengxiang){
+        this.fengxiang=fengxiang;
+    }
+    public void setPm25(String pm25) {
+        this.pm25 = pm25;
+    }
+
+    public void setShidu(String shidu) {
+
+        this.shidu = shidu;
+    }
+
+    public void setWendu(String wendu) {
+
+        this.wendu = wendu;
+    }
+
+    public void setUpdatetime(String updatetime) {
+
+        this.updatetime = updatetime;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "TodayWeather{" +
+                "city='" + city + '\'' +
+                ",updatetime='" + updatetime + '\'' +
+                ",shidu='" + shidu + '\'' +
+                ",pm25='" + pm25 + '\'' +
+                ",quality='" + quality + '\'' +
+                ",fengxiang='" + fengxiang + '\'' +
+                ",fengli='" + fengli + '\'' +
+                ",date='" + date + '\'' +
+                ",high='" + high + '\'' +
+                ",low='" + low + '\'' +
+                ",type='" + type + '\'' +
+                '}';
+    }
+
 }
--- app/src/main/java/cn/edu/pku/chengxiaoxiao/weather/SelectCtiy.java
+++ app/src/main/java/cn/edu/pku/chengxiaoxiao/weather/SelectCtiy.java
@@ -1,8 +1,44 @@
 package cn.edu.pku.chengxiaoxiao.weather;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
 /**
  * Created by Jason on 16/10/23.
  */
 
-public class SelectCtiy {
+public class SelectCtiy extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView mBackBtn;
+    @Override
+    protected void onCreate(Bundle savedIntanceState){
+        super.onCreate(savedIntanceState);
+
+        setContentView(R.layout.select_city);
+
+
+        mBackBtn=(ImageView)findViewById(R.id.title_back);
+        mBackBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.title_back:
+
+                Intent i = new Intent();
+                i.putExtra("cityCode", "101160101");
+                setResult(RESULT_OK, i);
+
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
 }
